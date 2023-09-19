7e9952f955fd827c9e56d676ad4bf6db7ebd2b9a
phat <minhlee17999@gmail.com>
2023-09-19 09:33:51 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -16,31 +16,30 @@ var firebaseConfig = {
 };
 
 initializeApp(firebaseConfig);
-function notifyMe() {
-  if (!("Notification" in window)) {
-    // Check if the browser supports notifications
-    alert("This browser does not support desktop notification");
-  } else if (Notification.permission === "granted") {
-    // Check whether notification permissions have already been granted;
-    // if so, create a notification
-    const notification = new Notification("Hi there!");
-    // …
-  } else if (Notification.permission !== "denied") {
-    // We need to ask the user for permission
-    Notification.requestPermission().then((permission) => {
-      // If the user accepts, let's create a notification
-      if (permission === "granted") {
-        const notification = new Notification("Hi there!");
-        // …
-      }
-    });
-  }
-}
+
 const messaging = getMessaging();
 async function App() {
-  notifyMe();
   const [token, setToken] = useState("");
-
+  function notifyMe() {
+    if (!("Notification" in window)) {
+      // Check if the browser supports notifications
+      alert("This browser does not support desktop notification");
+    } else if (Notification.permission === "granted") {
+      // Check whether notification permissions have already been granted;
+      // if so, create a notification
+      const notification = new Notification("Hi there!");
+      // …
+    } else if (Notification.permission !== "denied") {
+      // We need to ask the user for permission
+      Notification.requestPermission().then((permission) => {
+        // If the user accepts, let's create a notification
+        if (permission === "granted") {
+          const notification = new Notification("Hi there!");
+          // …
+        }
+      });
+    }
+  }
   if (Notification.permission === "granted") {
     console.log("allow", result);
     return true;
@@ -94,6 +93,7 @@ async function App() {
         </a>
       </header>
       <Notification />
+      <button onClick={notifyMe()}>Get permission</button>
     </div>
   );
 }
