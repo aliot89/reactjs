6588892074ca11a0a3fa6564d148993aba7d2a41
phat <minhlee17999@gmail.com>
2023-09-19 14:09:26 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,80 +1,76 @@
-import logo from "./logo.svg";
-import "./App.css";
-import Notification from "./firebaseNotifications/Notification";
-import { useState } from "react";
-import { initializeApp } from "firebase/app";
-import { getMessaging, getToken, onMessage } from "firebase/messaging";
+import React from 'react';
+import logo from './logo.svg';
+import usePushNotifications from './usePushNotifications';
+import './App.css';
 
-var firebaseConfig = {
-  apiKey: "AIzaSyCJ_YIzbq2PDVB1SvAwcflvN4bnqN00vy4",
-  authDomain: "tesstts.firebaseapp.com",
-  projectId: "tesstts",
-  storageBucket: "tesstts.appspot.com",
-  messagingSenderId: "130388392879",
-  appId: "1:130388392879:web:9dacb10254ab240c910d5a",
-  measurementId: "G-D1PLR86NEY",
-};
-
-initializeApp(firebaseConfig);
-
-const messaging = getMessaging();
-async function requestPermisson() {
-  // let permission = await window.Notification.requestPermission();
-  if ("Notification" in window) {
-    window.Notification.requestPermission()
-      .then((permission) => {
-        alert(
-          "Notification permission granted. Requesting for token.",
-          permission
-        );
-      })
-      .catch((error) => {
-        alert(error);
-      });
-  } else {
-    // Thông báo rằng trình duyệt không hỗ trợ thông báo
-  }
-}
 function App() {
-  const [token, setToken] = useState("");
-  getToken(messaging, {
-    vapidKey:
-      "BNiYast8NllLtbCmjB7tEy1Ja95lcKdr0_Unmz41P96-c5OHtqq1L60fhrlOGY2hW3RQDNdoVoF5MwLHUg2UlnQ",
-  })
-    .then((currentToken) => {
-      if (currentToken) {
-        console.log("current token for client: ", currentToken);
-        // Perform any other neccessary action with the token
-        setToken(currentToken);
-        return currentToken;
-      } else {
-        // Show permission request UI
-        console.log(
-          "No registration token available. Request permission to generate one."
-        );
-      }
-    })
-    .catch((err) => {
-      console.log("An error occurred while retrieving token. ", err);
-    });
+  const {
+    userConsent,
+    pushNotificationSupported,
+    userSubscription,
+    onClickAskUserPermission,
+    onClickSusbribeToPushNotification,
+    onClickSendSubscriptionToPushServer,
+    pushServerSubscriptionId,
+    onClickSendNotification,
+    error,
+    loading
+  } = usePushNotifications();
+
+  const Loading = ({ loading }) =>
+    loading ? <div className='app-loader'>Please wait, we are loading something...</div> : null;
+  const Error = ({ error }) =>
+    error ? (
+      <section className='app-error'>
+        <h2>{error.name}</h2>
+        <p>Error message : {error.message}</p>
+        <p>Error code : {error.code}</p>
+      </section>
+    ) : null;
+
+  const isConsentGranted = userConsent === 'granted';
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
+        <Loading loading={loading} />
+
+        <p>Push notification are {!pushNotificationSupported && 'NOT'} supported by your device.</p>
+
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          User consent to recevie push notificaitons is <strong>{userConsent}</strong>.
         </p>
-        <a
-          className="App-link"
-          href={token}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          {token}
-        </a>
+
+        <Error error={error} />
+
+        <button
+          disabled={!pushNotificationSupported || isConsentGranted}
+          onClick={onClickAskUserPermission}>
+          {isConsentGranted ? 'Consent granted' : ' Ask user permission'}
+        </button>
+
+        <button
+          disabled={!pushNotificationSupported || !isConsentGranted || userSubscription}
+          onClick={onClickSusbribeToPushNotification}>
+          {userSubscription ? 'Push subscription created' : 'Create Notification subscription'}
+        </button>
+
+        <button
+          disabled={!userSubscription || pushServerSubscriptionId}
+          onClick={onClickSendSubscriptionToPushServer}>
+          {pushServerSubscriptionId
+            ? 'Subscrption sent to the server'
+            : 'Send subscription to push server'}
+        </button>
+
+        {pushServerSubscriptionId && (
+          <div>
+            <p>The server accepted the push subscrption!</p>
+            <button onClick={onClickSendNotification}>Send a notification</button>
+          </div>
+        )}
       </header>
-      <Notification />
-      <button onClick={requestPermisson}>tesst</button>
     </div>
   );
 }
