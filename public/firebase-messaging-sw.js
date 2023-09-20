c1b0d4dff39873381c7a326d28001687ee0e4c34
phat <minhlee17999@gmail.com>
2023-09-20 11:01:10 +0700

    first commit

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,8 +1,8 @@
 //public/firebase-messaging-sw.js
-importScripts('https://www.gstatic.com/firebasejs/8.2.0/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/8.2.0/firebase-messaging.js');
+importScripts("https://www.gstatic.com/firebasejs/8.2.0/firebase-app.js");
+importScripts("https://www.gstatic.com/firebasejs/8.2.0/firebase-messaging.js");
 
-self.addEventListener('fetch', () => {
+self.addEventListener("fetch", () => {
   const urlParams = new URLSearchParams(location.search);
   self.firebaseConfig = Object.fromEntries(urlParams);
 });
@@ -12,12 +12,13 @@ const defaultConfig = {
   projectId: true,
   messagingSenderId: true,
   appId: true,
+  image: "./logo192.png",
 };
 
 firebase.initializeApp(self.firebaseConfig || defaultConfig);
 if (firebase.messaging.isSupported()) {
   const messaging = firebase.messaging();
-  const channel = new BroadcastChannel('notifications');
+  const channel = new BroadcastChannel("notifications");
   messaging.onBackgroundMessage(function (payload) {
     //can not console.log here
     channel.postMessage(payload);
